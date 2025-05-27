dab68594fe3f487da3b329c65dd61d41758a9b84
Jonatas Oliveira <jonatasoliveiramont@gmail.com>
2025-05-27 11:33:54 -0300

    first commit

---
+++ src/test/java/com/devsuperior/webservicesjpa/WebservicesjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.webservicesjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
