18c35038a45e773889845476c4ede9cdc480bd6e
Jonatas Oliveira <jonatasoliveiramont@gmail.com>
2025-05-28 19:42:05 -0300

    Create insert, update and delete product methods

--- src/main/java/com/devsuperior/webservicesjpa/resources/ProductResource.java
+++ src/main/java/com/devsuperior/webservicesjpa/resources/ProductResource.java
@@ -4,11 +4,10 @@ import com.devsuperior.webservicesjpa.entities.Product;
 import com.devsuperior.webservicesjpa.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,5 +25,24 @@ public class ProductResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Product> findById(@PathVariable Long id) {
         return ResponseEntity.ok().body(productService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> insert(@RequestBody Product product) {
+        product = productService.insert(product);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(product.getId()).toUri();
+        return ResponseEntity.created(uri).body(product);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id,
+                                          @RequestBody Product product) {
+        return ResponseEntity.ok().body(productService.update(id, product));
     }
 }
--- src/main/java/com/devsuperior/webservicesjpa/services/ProductService.java
+++ src/main/java/com/devsuperior/webservicesjpa/services/ProductService.java
@@ -2,7 +2,11 @@ package com.devsuperior.webservicesjpa.services;
 
 import com.devsuperior.webservicesjpa.entities.Product;
 import com.devsuperior.webservicesjpa.repositories.ProductRepository;
+import com.devsuperior.webservicesjpa.services.exceptions.DatabaseException;
+import com.devsuperior.webservicesjpa.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +24,36 @@ public class ProductService {
 
     public Product findById(Long id) {
         Optional<Product> product = productRepository.findById(id);
-        return product.get();
+        return product.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Product insert(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void delete(Long id) {
+        if (!productRepository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+        try {
+            productRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Product update(Long id, Product product) {
+        try {
+            Product entity = productRepository.getReferenceById(id);
+
+            entity.setName(product.getName() != null ? product.getName() : entity.getName());
+            entity.setDescription(product.getDescription() != null ? product.getDescription() : entity.getDescription());
+            entity.setPrice(product.getPrice() != null ? product.getPrice() : entity.getPrice());
+            entity.setImgUrl(product.getImgUrl() != null ? product.getImgUrl() : entity.getImgUrl());
+
+            return entity;
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 }
