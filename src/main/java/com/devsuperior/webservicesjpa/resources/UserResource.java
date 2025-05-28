55eff74b0eb8443dde10339ab4ea0f4a70e72f8b
Jonatas Oliveira <jonatasoliveiramont@gmail.com>
2025-05-28 16:45:47 -0300

    Create insert, update and delete user methods

--- src/main/java/com/devsuperior/webservicesjpa/resources/UserResource.java
+++ src/main/java/com/devsuperior/webservicesjpa/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.devsuperior.webservicesjpa.entities.User;
 import com.devsuperior.webservicesjpa.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,12 +18,30 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<User>> findAll() {
         return ResponseEntity.ok().body(userService.findAll());
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
+    public ResponseEntity<User> findById(@PathVariable Long id) {
         return ResponseEntity.ok().body(userService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        return ResponseEntity.ok().body(userService.update(id, user));
     }
 }
--- src/main/java/com/devsuperior/webservicesjpa/services/UserService.java
+++ src/main/java/com/devsuperior/webservicesjpa/services/UserService.java
@@ -3,7 +3,6 @@ package com.devsuperior.webservicesjpa.services;
 import com.devsuperior.webservicesjpa.entities.User;
 import com.devsuperior.webservicesjpa.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,11 +14,31 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return userRepository.findAll();
     }
-    public User findById(Long id){
+
+    public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
         return user.get();
+    }
+
+    public User insert(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public User update(Long id, User user) {
+        User entity = userRepository.getReferenceById(id);
+
+        entity.setName(user.getName() != null ? user.getName() : entity.getName());
+        entity.setEmail(user.getEmail() != null ? user.getEmail() : entity.getEmail());
+        entity.setPassword(user.getPassword() != null ? user.getPassword() : entity.getPassword());
+        entity.setPhone(user.getPhone() != null ? user.getPhone() : entity.getPhone());
+
+        return userRepository.save(entity);
     }
 }
