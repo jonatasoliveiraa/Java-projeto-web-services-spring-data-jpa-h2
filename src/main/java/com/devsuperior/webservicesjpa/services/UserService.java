31095769a43595d9b53580b97853904d6d28fa27
Jonatas Oliveira <jonatasoliveiramont@gmail.com>
2025-05-28 17:26:59 -0300

    Create ResourceNotFoundException

--- src/main/java/com/devsuperior/webservicesjpa/services/UserService.java
+++ src/main/java/com/devsuperior/webservicesjpa/services/UserService.java
@@ -2,6 +2,7 @@ package com.devsuperior.webservicesjpa.services;
 
 import com.devsuperior.webservicesjpa.entities.User;
 import com.devsuperior.webservicesjpa.repositories.UserRepository;
+import com.devsuperior.webservicesjpa.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
